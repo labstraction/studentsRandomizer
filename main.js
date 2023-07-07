66dbc9e815c6257c7e5c63234b5d32e495aa4157
labstraction <45163263+labstraction@users.noreply.github.com>
2023-07-07 12:39:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -69,3 +69,10 @@ function shuffle(){
     class1.shuffleStudents();
     renderPage(3);
 }
+
+
+//// Compito per il fine settimana:
+//1) Creare una applicazione che mostri una lista di videogiochi/film/libri(quello che volete)
+//// e che possa essere messa in ordine per nome o anno di pubblicazione
+
+//2) Capitoli 13 / 14 di eloquent js
